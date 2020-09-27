558f53e5843fbf7ce655f4f2bbf3a0b36d71bccf
Bala <labarefo2015@gmail.com>
2020-09-27 20:32:08 +0200

    Dismissible

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,16 @@ class _MyHomePageState extends State<MyHomePage> {
     new Activite("Golf", Icons.golf_course),
     new Activite("Arcade", Icons.gamepad),
     new Activite("Bricolage", Icons.build),
+    new Activite("Vélo", Icons.directions_bike),
+    new Activite("Peinture", Icons.palette),
+    new Activite("Golf", Icons.golf_course),
+    new Activite("Arcade", Icons.gamepad),
+    new Activite("Bricolage", Icons.build),
+    new Activite("Vélo", Icons.directions_bike),
+    new Activite("Peinture", Icons.palette),
+    new Activite("Golf", Icons.golf_course),
+    new Activite("Arcade", Icons.gamepad),
+    new Activite("Bricolage", Icons.build),
   ];
 
   @override
@@ -52,11 +62,33 @@ class _MyHomePageState extends State<MyHomePage> {
         child: new ListView.builder(
           itemCount: mesActivites.length,
             itemBuilder: (context, i){
-          return new ListTile(
-            title: new Text("Activité:  ${mesActivites[i].nom}"),
-            trailing: new Icon(mesActivites[i].icone),
-            leading: new Icon(mesActivites[i].icone),
+            Activite activite = mesActivites[i];
+            var key = activite.nom;
+          return new Dismissible(
+              key: new Key(key),
+              background: new Container(
+                padding: EdgeInsets.only(right: 20.0),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    new Text("Supprimer", style: new TextStyle(color: Colors.white),),
+                    new Icon(Icons.delete, color: Colors.white,),
+                  ],
+                ),
+                color: Colors.red,
+              ),
+              onDismissed: (direction){
+                setState(() {
+                  print(activite.nom);
+                  mesActivites.removeAt(i);
+                });
+              },
+              child: new ListTile(
+                title: new Text("Activité:  ${activite.nom}"),
+                trailing: new Icon(activite.icone),
+              )
           );
+
         }),
 
       ),
